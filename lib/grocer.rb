17537b93c0a59330e70a34f243323a3f1d277d39
Blake Runyon <blakerunyon09@gmail.com>
2021-01-06 17:35:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,39 @@
+list = [
+    {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+    {:item => "AVOCADO", :price => 3.00, :clearance => true},
+    {:item => "AVOCADO", :price => 3.00, :clearance => true},
+    {:item => "KALE", :price => 3.00, :clearance => false},
+    {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+    {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+    {:item => "ALMONDS", :price => 9.00, :clearance => false},
+  ]
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  collection.map { |items|
+    items.map { |k, v|
+      if v === name
+        return item_hash = {k => v}
+      end
+    }
+  }
+nil
 end
 
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+def consolidate_cart(old_cart)
+new_cart = []
+old_cart.map { |e_old|
+  if !find_item_by_name_in_collection(e_old[:item], new_cart)
+    new_cart.push(e_old)
+    e_old[:count] = 1
+  else
+    new_cart.map { |e|
+      if e_old[:item] === e[:item]
+      e[:count] += 1
+      end
+    }
+  end
+}
+pp new_cart
 end
 
-
-  
+consolidate_cart(list)
